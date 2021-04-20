5ad0591d39001fc742fc91b2c2a85584dadcdfc3
neminem1203 <tpaul1203@gmail.com>
2021-04-20 19:04:34 -0400

    sorting of skills

--- src/App.js
+++ src/App.js
@@ -13,18 +13,63 @@ const projectX = 30;
 const projectY = 40;
 const offSet = 70;
 
+
+function alphabeticalSort(a, b){
+  return a[0].charCodeAt(0) - b[0].charCodeAt(0);
+}
+
+function reverseAlpbaeticalSort(a,b){
+  return b[0].charCodeAt(0) - a[0].charCodeAt(0);
+}
+
+function proficiencySort(a, b){
+  return skills[b] - skills[a];
+}
+
+function reverseProficienySort(a,b){
+  return skills[a] - skills[b];
+}
+
+
 function App() {
   const [search, setSearch] = useState("");
-  const skillset = Object.keys(skills).map(skill_name => {
-            let opacity = 0.5;
-            const reg_ex = new RegExp(search.replace(/[#-.]|[[-^]|[?|{}]/g, '\\$&').toLowerCase());
-            if(search == "" || reg_ex.test(skill_name.toLowerCase())){
-              opacity = 1;
-            }
-            return <div style={{opacity: opacity}}>
-              {skill_name} : {skill_levels[skills[skill_name]]}
-              </div>
-          })
+  const [sortState, setSortState] = useState(0);
+  const sortFuncs = [alphabeticalSort,reverseAlpbaeticalSort, proficiencySort,reverseProficienySort];
+  let skill_list = Object.keys(skills);
+  let buttonClass1 = "sort-buttons";
+  let buttonClass2 = "sort-buttons";
+  let buttonClass3 = "sort-buttons";
+  switch(sortState){
+    case 0:
+      buttonClass1 += " selected";
+      break;
+    case 1:
+      buttonClass2 += " selected";
+      break;
+    case 2:
+      buttonClass2 += " reversed";
+      break;
+    case 3:
+      buttonClass3 += " selected";
+      break
+    case 4:
+      buttonClass3 += " reversed";
+      break;
+  }
+  if(sortState > 0){
+    skill_list.sort(sortFuncs[sortState-1])
+  
+  }
+  const skillset = skill_list.map(skill_name => {
+              let opacity = 0.5;
+              const reg_ex = new RegExp(search.replace(/[#-.]|[[-^]|[?|{}]/g, '\\$&').toLowerCase());
+              if(search == "" || reg_ex.test(skill_name.toLowerCase())){
+                opacity = 1;
+              }
+              return <div style={{opacity: opacity}}>
+                {skill_name} : {skill_levels[skills[skill_name]]}
+                </div>
+            })
   return (
     <div className="App">
       <div className="bookshelf"> 
@@ -50,12 +95,22 @@ function App() {
         <h1>Paul Tan</h1>
         <h3>Software Developer</h3>
         <p>Hey! Welcome to my portfolio site! You can drag my portfolio sites around and double click to open them for more information with live links and github links</p>
-        {/* <div className="skill-legend">
-        {Object.keys(skill_levels).map(num => {return <div>
+        <div className="skill-legend">
+        {/* {Object.keys(skill_levels).map(num => {return <div>
             {num} : {skill_levels[num]}
           </div>
-          })}
-        </div> */}
+          })} */}
+          <h1>Sort By</h1>
+          <div className={buttonClass1} onClick={()=>setSortState(0)} >Unsorted</div>
+          <div className={buttonClass2} onClick={()=>{
+            let newSortState = sortState == 1 ? 2 : 1;
+            setSortState(newSortState);
+          }}>Alphabetical</div>
+          <div className={buttonClass3} onClick={()=>{
+            let newSortState = sortState == 3 ? 4 : 3;
+            setSortState(newSortState);
+          }}>Proficiency</div>
+        </div>
         <div>
           <input type="text" onChange={(e)=>{setSearch(e.currentTarget.value)}}></input>
         </div>
